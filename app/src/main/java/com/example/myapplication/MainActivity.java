9492c0256bb5b8070b53bf22f81d2d0e50f8c616
ViniciusCamposAraujo <vinicius.araujo@foursys.com.br>
2023-10-05 03:52:14 -0300

    update tela de login e cadastro (+animation)

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -3,12 +3,94 @@ package com.example.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import com.google.android.material.button.MaterialButton;
+import com.google.android.material.button.MaterialButtonToggleGroup;
+import com.google.android.material.textfield.TextInputLayout;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextInputLayout emailLogin;
+    private TextInputLayout senhaLogin;
+    private TextView esqueceuSenha;
+    private TextInputLayout emailCadastro;
+    private TextInputLayout senhaCadastro;
+    private TextInputLayout confirmarSenhaCadastro;
+    private Animation fadeInAnimation;
+    private Animation fadeOutAnimation;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        initializeViews();
+        initializeAnimations();
+
+        MaterialButtonToggleGroup materialButtonToggleGroup = findViewById(R.id.btnsToggle);
+
+        materialButtonToggleGroup.addOnButtonCheckedListener(new MaterialButtonToggleGroup.OnButtonCheckedListener() {
+            @Override
+            public void onButtonChecked(MaterialButtonToggleGroup group, int checkedId, boolean isChecked) {
+
+                if (isChecked) {
+                    if (checkedId == R.id.btnLogin) {
+                        setVisibilityWithAnimation(View.VISIBLE, View.GONE);
+                    } else if (checkedId == R.id.btnCadastrar) {
+                        setVisibilityWithAnimation(View.GONE, View.VISIBLE);
+                    }
+                }
+            }
+        });
     }
+
+    private void initializeViews() {
+        //       -------------- login------------------
+        emailLogin = findViewById(R.id.emailLogin);
+        senhaLogin = findViewById(R.id.senhaLogin);
+        esqueceuSenha = findViewById(R.id.esqueceuSenha);
+        //       -------------- Cadastro------------------
+        emailCadastro = findViewById(R.id.emailCadastro);
+        senhaCadastro = findViewById(R.id.senhaCadastro);
+        confirmarSenhaCadastro = findViewById(R.id.confirmarSenhaCadastro);
+    }
+
+    private void initializeAnimations() {
+        fadeInAnimation = AnimationUtils.loadAnimation(this, R.anim.fade_in);
+        fadeOutAnimation = AnimationUtils.loadAnimation(this, R.anim.fade_out);
+    }
+
+    private void setVisibilityWithAnimation(int loginVisibility, int cadastroVisibility) {
+        if (loginVisibility == View.VISIBLE) {
+            emailLogin.startAnimation(fadeInAnimation);
+            senhaLogin.startAnimation(fadeInAnimation);
+            esqueceuSenha.startAnimation(fadeInAnimation);
+        } else {
+            emailLogin.startAnimation(fadeOutAnimation);
+            senhaLogin.startAnimation(fadeOutAnimation);
+            esqueceuSenha.startAnimation(fadeOutAnimation);
+        }
+
+        if (cadastroVisibility == View.VISIBLE) {
+            emailCadastro.startAnimation(fadeInAnimation);
+            senhaCadastro.startAnimation(fadeInAnimation);
+            confirmarSenhaCadastro.startAnimation(fadeInAnimation);
+        } else {
+            emailCadastro.startAnimation(fadeOutAnimation);
+            senhaCadastro.startAnimation(fadeOutAnimation);
+            confirmarSenhaCadastro.startAnimation(fadeOutAnimation);
+        }
+
+        emailLogin.setVisibility(loginVisibility);
+        senhaLogin.setVisibility(loginVisibility);
+        esqueceuSenha.setVisibility(loginVisibility);
+        emailCadastro.setVisibility(cadastroVisibility);
+        senhaCadastro.setVisibility(cadastroVisibility);
+        confirmarSenhaCadastro.setVisibility(cadastroVisibility);
+    }
+
 }
